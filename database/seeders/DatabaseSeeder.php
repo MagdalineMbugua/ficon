c0f95759c21f01d9afdc2cf05102daf5db915ef2
Magdaline Mbugua <66258194+MagdalineMbugua@users.noreply.github.com>
2023-07-05 18:27:44 +0300

    Add roles and permissions (#3)
    Co-authored-by: Magdaline Mbugua <empressmagda@Magdalines-MacBook-Pro.local>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        $this->call([
+            RolesAndPermissionSeeder::class,
+        ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
